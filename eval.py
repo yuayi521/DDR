6dd08b454bcec3fb4dd59c637d7cdf11e9e0c735
yuquanjie <yuquanjie13@gmail.com>
2017-10-11 18:28:29 +0800

    commit sort_poly, because it maybe generate not clock-wise text box

--- eval.py
+++ eval.py
@@ -7,10 +7,10 @@ import model
 import matplotlib.pyplot as plt
 import locality_aware_nms as nms_locality
 
-# tf.app.flags.DEFINE_string('test_data_path', '/data/ocr/icdar2015/', '')
 tf.app.flags.DEFINE_string('test_data_path', '/data/ocr/ch4/', '')
+# tf.app.flags.DEFINE_string('test_data_path', '/data/ocr/sp_case/', '')
 tf.app.flags.DEFINE_string('gpu_list', '0', '')
-tf.app.flags.DEFINE_string('checkpoint_path', '/tmp/ddr_icdar15/', '')
+tf.app.flags.DEFINE_string('checkpoint_path', '/tmp/ddr_icdar15_shrinkedpoly/', '')
 tf.app.flags.DEFINE_string('output_path', '/home/yuquanjie/output_ocr/', '')
 FLAGS = tf.app.flags.FLAGS
 
@@ -95,47 +95,43 @@ def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_
         geo_map = geo_map[0, :, :, ]
     # filter the score map
     xy_text = np.argwhere(score_map > score_map_thresh)
-    # visualize the score map
-    if vis:
-        plt.imshow(score_map)
-        plt.show()
+    # plt.imshow(score_map)
+    # plt.show()
     # sort the text boxes via the y axis
     xy_text = xy_text[np.argsort(xy_text[:, 0])]
     # restore text boxes from score_map and geo_map
     start = time.time()
     text_box_restored = restore_rectangle(xy_text[:, ::-1] * 4, geo_map[xy_text[:, 0], xy_text[:, 1], :])  # N*4*2
     print '{} text boxes before nms'.format(text_box_restored.shape[0])
-
-    # boxes = text_box_restored
-    # score_map greater than threshold(0.8)
-    # points = xy_text[:, ::-1] * 4
-
     boxes = np.zeros((text_box_restored.shape[0], 9), dtype=np.float32)
     boxes[:, :8] = text_box_restored.reshape((-1, 8))
     boxes[:, 8] = score_map[xy_text[:, 0], xy_text[:, 1]]
     timer['restore'] = time.time() - start
-
     # nms part
     start = time.time()
     boxes = nms_locality.nms_locality(boxes.astype(np.float64), nms_thres)
     timer['nms'] = time.time() - start
-
     if boxes.shape[0] == 0:
         return None, timer
-        # return None, None, timer
-
     # here we filter some low score boxes by the average score map, this is different from the orginal paper
     for i, box in enumerate(boxes):
         mask = np.zeros_like(score_map, dtype=np.uint8)
         cv2.fillPoly(mask, box[:8].reshape((-1, 4, 2)).astype(np.int32) / 4, 1)
         boxes[i, 8] = cv2.mean(score_map, mask)[0]
     boxes = boxes[boxes[:, 8] > box_thresh]
-
-    # return boxes, points.astype(np.float32), timer
     return boxes, timer
 
 
 def sort_poly(p):
+    """
+
+    Args:
+        p:
+
+    Returns:
+
+    """
+    # calculate value predicted text box's 4 points' x added y, then get the minimal value's index
     min_axis = np.argmin(np.sum(p, axis=1))
     p = p[[min_axis, (min_axis + 1) % 4, (min_axis + 2) % 4, (min_axis + 3) % 4]]
     if abs(p[0, 0] - p[1, 0]) > abs(p[0, 1] - p[1, 1]):
@@ -159,14 +155,13 @@ def main(argv=None):
             saver.restore(sess, model_path)
             im_fn_list = get_images()
             for im_fn in im_fn_list:
-                print '====>{}'.format(im_fn)
+                print im_fn
                 im = cv2.imread(im_fn)[:, :, ::-1]
                 im_resized, (ratio_h, ratio_w) = resize_image(im)
                 timer = {'net': 0, 'restore': 0, 'nms': 0}
                 start = time.time()
                 score, geometry = sess.run([f_score, f_geometry], feed_dict={input_images: [im_resized]})
                 timer['net'] = time.time() - start
-                # boxes, points, timer = detect(score_map=score, geo_map=geometry, timer=timer)
                 boxes, timer = detect(score_map=score, geo_map=geometry, timer=timer)
                 print '{} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
                     im_fn, timer['net'] * 1000, timer['restore'] * 1000, timer['nms'] * 1000)
@@ -176,12 +171,11 @@ def main(argv=None):
                     boxes[:, :, 1] /= ratio_h
                 # save to file
                 if boxes is not None:
-                    with open(
-                            os.path.join(FLAGS.output_path, 'res_{}.txt'.format(os.path.basename(im_fn).split('.')[0])),
-                            'w') as f:
+                    with open(os.path.join(FLAGS.output_path, 'res_{}.txt'.format(os.path.basename(im_fn).split('.')[0])), 'w') as f:
                         for box in boxes:
                             # to avoid submitting errors
-                            box = sort_poly(box.astype(np.int32))
+                            # box = sort_poly(box.astype(np.int32))
+                            box = box.astype(np.int32)
                             if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3]-box[0]) < 5:
                                 continue
                             f.write('{},{},{},{},{},{},{},{}\r\n'.format(
@@ -189,12 +183,6 @@ def main(argv=None):
                             ))
                             cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True,
                                           color=(255, 255, 0), thickness=1)
-                            # break
-
-                        # points[:, 0] /= ratio_w
-                        # points[:, 1] /= ratio_h
-                        # for point in points:
-                        #     cv2.circle(im[:, :, ::-1], (point[0], point[1]), 1, color=(255, 0, 0))
                 cv2.imwrite(os.path.join(FLAGS.output_path, os.path.basename(im_fn)), im[:, :, ::-1])
 
 
