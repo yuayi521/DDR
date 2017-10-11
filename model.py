e0dae995d67d74418198236c1336fd21ff783bb8
yuquanjie <yuquanjie13@gmail.com>
2017-10-11 10:03:31 +0800

    add shrinked poly

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def unpool(inputs):
-    return tf.image.resize_bilinear(inputs, size=[tf.shape(inputs)[1]*2,  tf.shape(inputs)[2]*2])
+    return tf.image.resize_bilinear(inputs, size=[tf.shape(inputs)[1] * 2, tf.shape(inputs)[2] * 2])
 
 
 def mean_image_subtraction(images, means=[123.68, 116.78, 103.94]):
@@ -20,7 +20,7 @@ def mean_image_subtraction(images, means=[123.68, 116.78, 103.94]):
     '''
     num_channels = images.get_shape().as_list()[-1]
     if len(means) != num_channels:
-      raise ValueError('len(means) must match the number of channels')
+        raise ValueError('len(means) must match the number of channels')
     channels = tf.split(axis=3, num_or_size_splits=num_channels, value=images)
     for i in range(num_channels):
         channels[i] -= means[i]
@@ -28,9 +28,9 @@ def mean_image_subtraction(images, means=[123.68, 116.78, 103.94]):
 
 
 def model(images, weight_decay=1e-5, is_training=True):
-    '''
+    """
     define the model, we use slim's implemention of resnet
-    '''
+    """
     images = mean_image_subtraction(images)
 
     with slim.arg_scope(resnet_v1.resnet_arg_scope(weight_decay=weight_decay)):
@@ -38,10 +38,10 @@ def model(images, weight_decay=1e-5, is_training=True):
 
     with tf.variable_scope('feature_fusion', values=[end_points.values]):
         batch_norm_params = {
-        'decay': 0.997,
-        'epsilon': 1e-5,
-        'scale': True,
-        'is_training': is_training
+            'decay': 0.997,
+            'epsilon': 1e-5,
+            'scale': True,
+            'is_training': is_training
         }
         with slim.arg_scope([slim.conv2d],
                             activation_fn=tf.nn.relu,
@@ -59,7 +59,7 @@ def model(images, weight_decay=1e-5, is_training=True):
                 if i == 0:
                     h[i] = f[i]
                 else:
-                    c1_1 = slim.conv2d(tf.concat([g[i-1], f[i]], axis=-1), num_outputs[i], 1)
+                    c1_1 = slim.conv2d(tf.concat([g[i - 1], f[i]], axis=-1), num_outputs[i], 1)
                     h[i] = slim.conv2d(c1_1, num_outputs[i], 3)
                 if i <= 2:
                     g[i] = unpool(h[i])
@@ -67,47 +67,16 @@ def model(images, weight_decay=1e-5, is_training=True):
                     g[i] = slim.conv2d(h[i], num_outputs[i], 3)
                 print('Shape of h_{} {}, g_{} {}'.format(i, h[i].shape, i, g[i].shape))
 
-            # here we use a slightly different way for regression part,
-            # we first use a sigmoid to limit the regression range, and also
-            # this is do with the angle map
-            # F_score = slim.conv2d(g[3], 1, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None)
-            # 4 channel of axis aligned bbox and 1 channel rotation angle
-            # geo_map = slim.conv2d(g[3], 4, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None) * FLAGS.text_scale
-            # angle_map = (slim.conv2d(g[3], 1, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None) - 0.5) * np.pi/2 # angle is between [-45, 45]
-            # F_geometry = tf.concat([geo_map, angle_map], axis=-1)
-
             f_cls = slim.conv2d(g[3], 1, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None)
-            # here we use a slightly different way for regression part,
-            # we first use a sigmoid to limit the regression range
-            # f_reg = slim.conv2d(g[3], 8, 1, activation_fn=tf.nn.sigmoid, normalizer_fn=None) * FLAGS.text_scale
+            # firstly, using a tanh to limit the regression range from -1 to 1, then multiplying image's size
             f_reg = slim.conv2d(g[3], 8, 1, activation_fn=tf.nn.tanh, normalizer_fn=None) * FLAGS.text_scale
-
-    # return F_score, F_geometry
     return f_cls, f_reg
 
 
-def dice_coefficient_remove_mask(y_true_cls, y_pred_cls):
-    """
-    dice loss
-    :param y_true_cls:
-    :param y_pred_cls:
-    :param training_mask:
-    :return:
-    """
-    eps = 1e-5
-    intersection = tf.reduce_sum(y_true_cls * y_pred_cls)
-    union = tf.reduce_sum(y_true_cls) + tf.reduce_sum(y_pred_cls) + eps
-    loss = 1. - (2 * intersection / union)
-    return loss
+def cls_loss(y_true_cls, y_pred_cls, training_mask):
 
+    y_true_cls = y_true_cls * training_mask
 
-def my_cls_loss(y_true_cls, y_pred_cls):
-    """
-
-    :param y_true_cls:
-    :param y_pred_cls:
-    :return:
-    """
     y_true_cls = tf.cast(y_true_cls, tf.float32)
     y_pred_cls = tf.cast(y_pred_cls, tf.float32)
 
@@ -118,19 +87,9 @@ def my_cls_loss(y_true_cls, y_pred_cls):
     return loss_cls
 
 
-def my_loss(y_true_cls, y_pred_cls, y_true_reg, y_pred_reg):
-    """
+def loss(y_true_cls, y_pred_cls, y_true_reg, y_pred_reg, training_mask):
 
-    :param y_pred_reg:
-    :param y_true_reg:
-    :param y_true_cls:
-    :param y_pred_cls:
-    :return:
-    """
-
-    loss_cls = my_cls_loss(y_true_cls, y_pred_cls)
-    # use czc's cls loss
-    # loss_cls = dice_coefficient_remove_mask(y_true_cls, y_pred_cls)
+    loss_cls = cls_loss(y_true_cls, y_pred_cls, training_mask)
     # scale the classification loss to match regression loss
     loss_cls *= 0.1
     tf.summary.scalar('classification_loss', loss_cls)
@@ -145,7 +104,12 @@ def my_loss(y_true_cls, y_pred_cls, y_true_reg, y_pred_reg):
     for i in xrange(7):
         y_true_cls = tf.concat([y_true_cls, iter_], axis=-1)
 
-    loss_reg = tf.where(tf.greater(y_true_cls, 0.0),
+    # expand the dimension of training_mask from (80, 80, 1)to (80, 80, 8)
+    iter_ = training_mask
+    for i in xrange(7):
+        training_mask = tf.concat([training_mask, iter_], axis=-1)
+
+    loss_reg = tf.where(tf.greater(y_true_cls * training_mask, 0.0),
                         smooth,
                         0.0 * smooth)
     # loss_reg = tf.reduce_sum(loss_reg)
@@ -159,51 +123,3 @@ def my_loss(y_true_cls, y_pred_cls, y_true_reg, y_pred_reg):
     return loss_all
 
 
-def dice_coefficient(y_true_cls, y_pred_cls, training_mask):
-    '''
-    dice loss
-    :param y_true_cls:
-    :param y_pred_cls:
-    :param training_mask:
-    :return:
-    '''
-    eps = 1e-5
-    intersection = tf.reduce_sum(y_true_cls * y_pred_cls * training_mask)
-    union = tf.reduce_sum(y_true_cls * training_mask) + tf.reduce_sum(y_pred_cls * training_mask) + eps
-    loss = 1. - (2 * intersection / union)
-    tf.summary.scalar('classification_dice_loss', loss)
-    return loss
-
-
-def loss(y_true_cls, y_pred_cls, y_true_geo, y_pred_geo, training_mask):
-    '''
-    define the loss used for training, contraning two part,
-    the first part we use dice loss instead of weighted logloss,
-    the second part is the iou loss defined in the paper
-    :param y_true_cls: ground truth of text
-    :param y_pred_cls: prediction os text
-    :param y_true_geo: ground truth of geometry
-    :param y_pred_geo: prediction of geometry
-    :param training_mask: mask used in training, to ignore some text annotated by ###
-    :return:
-    '''
-    classification_loss = dice_coefficient(y_true_cls, y_pred_cls, training_mask)
-    # scale classification loss to match the iou loss part
-    classification_loss *= 0.01
-
-    # d1 -> top, d2->right, d3->bottom, d4->left
-    d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
-    d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
-    area_gt = (d1_gt + d3_gt) * (d2_gt + d4_gt)
-    area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
-    w_union = tf.minimum(d2_gt, d2_pred) + tf.minimum(d4_gt, d4_pred)
-    h_union = tf.minimum(d1_gt, d1_pred) + tf.minimum(d3_gt, d3_pred)
-    area_intersect = w_union * h_union
-    area_union = area_gt + area_pred - area_intersect
-    L_AABB = -tf.log((area_intersect + 1.0)/(area_union + 1.0))
-    L_theta = 1 - tf.cos(theta_pred - theta_gt)
-    tf.summary.scalar('geometry_AABB', tf.reduce_mean(L_AABB * y_true_cls * training_mask))
-    tf.summary.scalar('geometry_theta', tf.reduce_mean(L_theta * y_true_cls * training_mask))
-    L_g = L_AABB + 20 * L_theta
-
-    return tf.reduce_mean(L_g * y_true_cls * training_mask) + classification_loss
